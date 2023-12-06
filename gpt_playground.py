047fdb7f5b867b2ec8e414480d676cfa4146ba4b
Dominik Wisniewski <dominikwisniewski022@gmail.com>
2023-12-06 22:48:36 +0100

    gpt playground init

---
+++ gpt_playground.py
@@ -0,0 +1,15 @@
+from transformers import pipeline
+import random
+generator = pipeline('text-generation', model='gpt2')
+current_sentence = ""
+num_gens = 100
+step_size = 3
+cur_tokens = 0
+num_steps = 4
+
+
+for i in range(num_steps):
+    cur_tokens += step_size
+    result = generator(current_sentence, max_length=cur_tokens, num_return_sequences=num_gens)
+    current_sentence = result[random.randint(0, num_gens - 1)]['generated_text']
+    print(current_sentence)
