c83233e3e122f4a4413d348269bfbfe899f7e54f
Dominik Wisniewski <dominikwisniewski022@gmail.com>
2024-02-16 18:33:37 +0100

    mcts rewritten

--- MCTS_sol.py
+++ MCTS_sol.py
@@ -1,49 +1,92 @@
 import random
 import math
-from transformers import pipeline
+from transformers import pipeline, set_seed
 from sentence_transformers import SentenceTransformer
+from argparse import ArgumentParser
+from tqdm import tqdm
+
+random.seed(42) # for reproducibility
 
 
-### OUR SETUP
-
+# OUR LLM + EMBEDDING SETUP
+set_seed(42)  # for reproducibility
 generator = pipeline("text-generation", model="gpt2")
-
-target_sentence = "bear attack"
-
 embedder = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-target_embedding = embedder.encode(target_sentence)
 
-MAX_LEN = 20
+# Hyperparemeters of the search - assigned with commandline arguments
+MAX_LEN = None
+step_size = None
+num_gens = None
+
+# Phrase we're looking for - assigned with commandline arguments
+target_phrase = None
+target_embedding = None
+
+# Best found state
 best_state = None
-best_score = -1.5
+best_score = -1.5 # score -1.5 as cos similiarity is in range [-1, 1]
 
-num_gens = 20
-step_size = 2
 
+"""
+    Node class in MCTS search tree.
+        state - string that the node represents
+        parent - pointer to parent node
+        children - pointers to children nodes identified by state
+        visits - number of visits to this node
+        eval - value in this node
+    Attributes visits and eval are essential to MCTS.
+"""
 class Node:
     def __init__(self, state, parent=None):
         self.state = state
         self.parent = parent
         self.children = {}
         self.visits = 0
-        self.value = eval(state)
+        self.value = 0 # Value for MCTS search
+        self.score = float(eval(state)) # Cos similarity for this sentence
 
 
+"""
+    We're selecting a child which scores max on Upper Confidence Bound (UCT) score
+"""
 def select(node):
     l = list(node.children.values())
     l.append(node)
     return max(l, key=uct_score)
 
 
-
-
+"""
+    Calculating UCT score.
+    If node is not visited - assign infinity to it, else calculate the score with formula
+    node_value / node_visits + sqrt(2 * ln(parent_visits) / node_visits)
+"""
 def uct_score(node):
     if node.visits == 0:
         return float("inf")
-    z = node.parent.visits if node.parent is not None else 1
+    parent_visits = node.parent.visits if node.parent is not None else 1
     return (node.value / node.visits) + math.sqrt(
-        2 * z / node.visits
+        2 * math.log(parent_visits) / node.visits
     )
+
+
+"""
+    Getting random child for our node. 
+    We're generating only few most possible sentence continuations, for two reasons:
+    1. Reducing search space.
+    2. Considering only most humanlike sentence continuations.
+"""
+def get_random_child(node):
+    result = generator(
+        node.state,  # sentence in node
+        max_length=len(node.state) + step_size,  # setting max length
+        num_return_sequences=num_gens,
+        pad_token_id=50256 # Does nothing - default value for gpt-2
+    )
+    child_state = random.choice(result)["generated_text"] # TODO: should this be weigthed by LLM perplexity?
+    child = Node(child_state, parent=node)
+    if child_state not in node.children:
+        node.children[child_state] = child
+    return node.children[child_state]
 
 
 def expand(node):
@@ -53,25 +96,16 @@ def expand(node):
 def simulate(node):
     while not is_terminal(node):
         node = get_random_child(node)
+    # Here node is terminal, so we set the value of the game to the same as of cos similarity
+    node.value = node.score
     return node.value
 
 
 def backpropagate(node, value):
     while node is not None:
         node.visits += 1
-        node.value = max(node.value, value)
+        node.value += value
         node = node.parent
-
-
-def get_random_child(node):
-    result = generator(
-        node.state, max_length=len(node.state) + step_size, num_return_sequences=num_gens
-    )
-    child_state = random.choice(result)["generated_text"]
-    child = Node(child_state, parent=node)
-    if child_state not in node.children:
-        node.children[child_state] = child
-    return node.children[child_state]
 
 
 def is_terminal(node):
@@ -79,9 +113,10 @@ def is_terminal(node):
 
 
 def eval(state):
-    global best_state, best_score
+    global best_score, best_state
     embedding = embedder.encode(state)
     score = embedding @ target_embedding
+    # Always when we eval state we check if this_state > best_state
     if score > best_score:
         best_state = state
         best_score = score
@@ -90,12 +125,12 @@ def eval(state):
 
 
 def mcts(root_state, iterations):
-    global best_state
-    root_node = Node(root_state)
-    best_state = root_state
+    global best_state, best_score
+    root_node = Node(root_state) # Create root_node
+    best_state = root_node.state
+    best_score = root_node.value
 
-    for iter in range(iterations):
-        print(f"iteration {iter}")
+    for _ in tqdm(range(iterations), desc="MCTS progress"):
         selected_node = select(root_node)
         if not is_terminal(selected_node):
             child_node = expand(selected_node)
@@ -106,11 +141,55 @@ def mcts(root_state, iterations):
     return root_node
 
 
-# Example usage:
-initial_state = ""
-root_node = mcts(initial_state, iterations=100)
-print("best state: ", best_state, " with similarity ", eval(best_state))
-def print_tree(node):
-    print(node.state)
-    for child in node.children.values():
-        print_tree(child)
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        prog="MCTS solution",
+        description="Finding human-understandable word minimizing cosine similarity with given embedding with the use of LLM and MCTS search.",
+    )
+    parser.add_argument(
+        "--iter-num", type=int, help="Hyperparameter to MCTS search.", default=1000
+    )
+    parser.add_argument(
+        "--max-len",
+        type=int,
+        help="Max length in characters for a searched phrase.",
+        default=20,
+    )
+    parser.add_argument(
+        "--step-size",
+        type=int,
+        help="How many characters should be generated at most in one step of MCTS.",
+        default=1,
+    )
+    parser.add_argument(
+        "--num-gens",
+        type=int,
+        help="How many sentence continuations should LLM propose",
+        default=10,
+    )
+    parser.add_argument(
+        "--target-phrase", type=str, help="Target phrase to decode.", required=True
+    )
+    args = parser.parse_args()
+
+    # INITIAL SETUP
+    MAX_LEN = args.max_len
+    step_size = args.step_size
+    num_gens = args.num_gens
+    target_phrase = (
+        args.target_phrase
+    )  # phrase that we want to obtain (unknown to the algorithm)
+    target_embedding = embedder.encode(
+        target_phrase
+    )  # embedding of target_phrase (known to the algorithm)
+
+    # START SEARCH
+    root_node = mcts("", iterations=args.iter_num)
+
+    # AFTER SEARCH FINISHES
+    print("best state: ", best_state, " with similarity ", eval(best_state))
+
+    # def print_tree(node):
+    #    print(node.state)
+    #    for child in node.children.values():
+    #        print_tree(child)
