f13692d7fed471ee7ee7b474385d2100dc8d43f4
Dominik Wisniewski <dominikwisniewski022@gmail.com>
2024-02-24 21:24:16 +0100

    format and typo

--- dumb_model.py
+++ dumb_model.py
@@ -1,14 +1,15 @@
 from typing import Iterable
 from abstractmodel import AbstractLabelModel
 
+
 class DumbModel(AbstractLabelModel):
     def __init__(self) -> None:
         super().__init__()
-    
+
     @property
     def name(self):
         return "dumb model"
-    
+
     def get_label(self, texts: Iterable[str]) -> str:
         super().get_label(texts)
         return "Terrible label :("
