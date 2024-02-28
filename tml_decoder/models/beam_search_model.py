700c0d7d28b1bdb409e7b2dddb20f4d16f7d69bd
Dominik Wisniewski <dominikwisniewski022@gmail.com>
2024-02-28 21:07:42 +0100

    implement beam search

---
+++ tml_decoder/models/beam_search_model.py
@@ -0,0 +1,56 @@
+from typing import Iterable
+from tml_decoder.encoders.abstract_encoder import AbstractEncoder
+from tml_decoder.generators.abstract_generator import AbstractGenerator
+from tml_decoder.models.abstract_model import AbstractLabelModel
+import numpy as np
+
+
+class BeamSearchModel(AbstractLabelModel):
+    def __init__(self, encoder: AbstractEncoder, generator: AbstractGenerator, iter_num=20, beam_width=5) -> None:
+        super().__init__()
+        self.encoder = encoder
+        self.generator = generator
+        self.iter_num = iter_num
+        self.beam_width = beam_width
+
+    @property
+    def name(self):
+        return f"MCTS model, encoder: {self.encoder.name}, generator: {self.generator.name}"
+
+    def get_embedding_to_revert(self, texts: Iterable[str]):
+        encoded_texts = [self.encoder.encode(text) for text in texts]
+        return self.encoder.average_embedding(encoded_texts)
+
+    def get_label(self, texts: Iterable[str]) -> str:
+        target_embedding = self.get_embedding_to_revert(texts)
+        return self.beam_search("", target_embedding, self.iter_num)
+    
+    def generate_next_states(self, state, target_embedding):
+        children = self.generator.generate(state)
+        scores = [self.encoder.similarity(self.encoder.encode(child), target_embedding) for child in children]
+        return children, scores
+
+    def beam_search(self, initial_state, target_embedding, iter_num):
+        best_state = initial_state
+        best_score = self.encoder.similarity(
+            self.encoder.encode(initial_state), target_embedding
+        )
+
+        beam = [best_state]
+
+        for _ in range(iter_num):
+            new_beam = []
+
+            for state in beam:
+                next_states, next_scores = self.generate_next_states(state)
+                top_indices = np.argsort(next_scores)[-self.beam_width:]
+                top_idx = top_indices[0]
+                if next_scores[top_idx] > best_score:
+                    best_score = next_scores[top_idx]
+                    best_state = next_states[top_idx]
+                new_beam.extend([(next_states[i], next_scores[i]) for i in top_indices])
+
+            new_beam = sorted(new_beam, key=lambda x: x[1], reverse=True)
+            beam = new_beam[:self.beam_width]
+        
+        return best_state
