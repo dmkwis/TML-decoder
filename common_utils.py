23a65d4231c7e5b09f18a1102848ebbd4a765624
Jasiek Busz <jasiekbusz@gmail.com>
2024-02-28 07:58:22 +0100

    feat: raise exceptions for not implemneted generators and encoders

--- common_utils.py
+++ common_utils.py
@@ -7,22 +7,25 @@ from gpt2_generator import GPT2Generator
 from miniLM_encoder import MiniLMEncoder
 
 
-
 ### If you're gonna use any embedder / generator make sure that they're initialized in this file
 ### This is in order not to get confused and use different model for different experiments
 ### In future if we want to allow for using different models for text generation and embeddings
 ### we can create here different initializers and eval functions sharing common interface
 
-random.seed(42) # for reproducibility
+random.seed(42)  # for reproducibility
 
 # OUR LLM + EMBEDDING SETUP
+
 
 def get_generator(name: str, *args, **kwargs) -> AbstractGenerator:
     if name == "gpt2":
         return GPT2Generator(*args, **kwargs)
-    return None
+
+    raise NotImplementedError(f"Generator {name} not implemented")
+
 
 def get_encoder(name: str, *args, **kwargs) -> AbstractEncoder:
     if name == "MiniLM":
         return MiniLMEncoder(*args, **kwargs)
-    return None
+
+    raise NotImplementedError(f"Encoder {name} not implemented")
