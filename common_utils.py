ae8a6073b869ba40a21ac32c1ba0d1bda0591872
Jasiek Busz <jasiekbusz@gmail.com>
2024-02-28 08:11:43 +0100

    feat: implement get_model and add docstrings

--- common_utils.py
+++ common_utils.py
@@ -1,9 +1,12 @@
+from typing import Any
 from sentence_transformers import SentenceTransformer
 import random
 from abstract_encoder import AbstractEncoder
 from abstract_generator import AbstractGenerator
+from abstract_model import AbstractLabelModel
 
 from gpt2_generator import GPT2Generator
+from mcts_model import MCTSModel
 from miniLM_encoder import MiniLMEncoder
 
 
@@ -17,15 +20,64 @@ random.seed(42)  # for reproducibility
 # OUR LLM + EMBEDDING SETUP
 
 
-def get_generator(name: str, *args, **kwargs) -> AbstractGenerator:
+def get_generator(name: str, *args: Any, **kwargs: Any) -> AbstractGenerator:
+    """
+    Returns a generator based on the given name.
+
+    Args:
+        name: The name of the generator.
+        *args: Variable length argument list.
+        **kwargs: Arbitrary keyword arguments.
+
+    Returns:
+        AbstractGenerator: An instance of the generator.
+
+    Raises:
+        NotImplementedError: If the generator with the given name is not implemented.
+    """
     if name == "gpt2":
         return GPT2Generator(*args, **kwargs)
 
     raise NotImplementedError(f"Generator {name} not implemented")
 
 
-def get_encoder(name: str, *args, **kwargs) -> AbstractEncoder:
+def get_encoder(name: str, *args: Any, **kwargs: Any) -> AbstractEncoder:
+    """
+    Returns an encoder based on the specified name.
+
+    Args:
+        name: The name of the encoder.
+        *args: Variable length argument list.
+        **kwargs: Arbitrary keyword arguments.
+
+    Returns:
+        AbstractEncoder: An instance of the encoder.
+
+    Raises:
+        NotImplementedError: If the specified encoder is not implemented.
+    """
     if name == "MiniLM":
         return MiniLMEncoder(*args, **kwargs)
 
     raise NotImplementedError(f"Encoder {name} not implemented")
+
+
+def get_model(name: str, *args: Any, **kwargs: Any) -> AbstractLabelModel:
+    """
+    Returns an instance of a model based on the given name.
+
+    Args:
+        name: The name of the model.
+        *args: Variable length argument list.
+        **kwargs: Arbitrary keyword arguments.
+
+    Returns:
+        AbstractLabelModel: An instance of the model.
+
+    Raises:
+        NotImplementedError: If the model with the given name is not implemented.
+    """
+    if name == "MCTS":
+        return MCTSModel(*args, **kwargs)
+
+    raise NotImplementedError(f"Model {name} not implemented")
