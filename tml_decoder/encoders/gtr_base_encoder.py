6f7e43948942b768f797b4f57f2514644cb8f672
Dominik Wisniewski <dominikwisniewski022@gmail.com>
2024-02-29 01:04:34 +0100

    normalization Gtr encoder avg encoding

--- tml_decoder/encoders/gtr_base_encoder.py
+++ tml_decoder/encoders/gtr_base_encoder.py
@@ -2,6 +2,7 @@ from typing import Any
 from tml_decoder.encoders.abstract_encoder import AbstractEncoder
 from sentence_transformers import SentenceTransformer, util
 from numpy import ndarray
+import numpy as np
 
 
 class GtrBaseEncoder(AbstractEncoder):
@@ -13,7 +14,8 @@ class GtrBaseEncoder(AbstractEncoder):
         return util.dot_score(veca, vecb)
     
     def average_embedding(self, embeddings: ndarray) -> ndarray:
-        return embeddings.mean(axis=0, keepdims=True)
+        res = embeddings.sum(axis=0, keepdims=True)
+        return res/np.linalg.norm(res)
     
     def encode(self, text: str) -> ndarray:
         result = self.encoder.encode(text)
