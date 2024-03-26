7baf59429e97b1cca9e442d9e7bbc0bdd563f2c3
Dominik Wisniewski <dominikwisniewski022@gmail.com>
2024-03-26 03:58:11 +0100

    implement guides

---
+++ tml_decoder/models/guides/random_guide.py
@@ -0,0 +1,9 @@
+from typing import List
+
+from numpy import ndarray
+from tml_decoder.models.guides.abstract_guide import AbstractGuide
+import random
+
+class RandomGuide(AbstractGuide):
+    def choose_next(self, propositions: List[str], target_embedding: ndarray) -> str:
+        return random.choice(propositions)
