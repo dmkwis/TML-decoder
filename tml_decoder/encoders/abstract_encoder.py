07db59442f08ca7e8cdd06fb8af566794438ee90
Dominik Wisniewski <dominikwisniewski022@gmail.com>
2024-03-26 03:57:53 +0100

    update abstract_encoder and mini_lm_encoder

--- tml_decoder/encoders/abstract_encoder.py
+++ tml_decoder/encoders/abstract_encoder.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod, abstractproperty
 from typing import Any, List
 from numpy import ndarray
 import numpy as np
+import torch
+
 
 class AbstractEncoder(ABC):
     @abstractmethod
@@ -24,6 +26,55 @@ class AbstractEncoder(ABC):
         encoded_batch = np.stack(self.encode_batch(texts))
         return self.average_embedding(encoded_batch)
 
+    @abstractmethod
+    def tokenize_text(self, text: str) -> List[int]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_unused_token(self) -> str:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def get_token_id(self, token: str) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_characteristic_tensor_for_token_id(self, token: str) -> torch.Tensor:
+        raise NotImplementedError
+
+    @abstractmethod
+    def freeze_weights(self) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def unfreeze_embedding_weigths(self) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_eot_token_id(self) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_parameters(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def raw_encode(
+        self, input_ids: torch.Tensor, attention_mask: torch.Tensor
+    ) -> torch.Tensor:
+        raise NotImplementedError
+
+    @abstractmethod
+    def zero_grad_for_embeddings(self, mask: torch.Tensor):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_tokenizer_vocab(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_embedding_for_token_id(self, token_id: int):
+        raise NotImplementedError
 
     @abstractproperty
     def name(self) -> str:
--- tml_decoder/encoders/mini_lm_encoder.py
+++ tml_decoder/encoders/mini_lm_encoder.py
@@ -1,13 +1,26 @@
 from typing import List
+
+import torch
 from tml_decoder.encoders.abstract_encoder import AbstractEncoder
 from sentence_transformers import SentenceTransformer, util
 import numpy as np
 from numpy import ndarray
 
+DEFAULT_DEVICE = None
+
+if torch.cuda.is_available():
+    DEFAULT_DEVICE = "cuda"
+elif torch.backends.mps.is_available():
+    DEFAULT_DEVICE = "mps"
+else:
+    DEFAULT_DEVICE = "cpu"
+
+
 class MiniLMEncoder(AbstractEncoder):
-    def __init__(self):
+    def __init__(self, device=DEFAULT_DEVICE):
         super().__init__()
-        self.encoder = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+        self.device = device
+        self.encoder = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2").to(self.device)
     
     def similarity(self, veca: ndarray, vecb: ndarray) -> float:
         return util.dot_score(veca, vecb).item()
@@ -26,6 +39,54 @@ class MiniLMEncoder(AbstractEncoder):
     def encode_batch(self, texts: List[str]) -> List[ndarray]:
         return self.encoder.encode(texts)
     
+
+    def tokenize_text(self, text: str) -> List[int]:
+        return self.encoder.tokenizer.encode(text)
+
+    def get_unused_token(self) -> str:
+        return next(
+            k for k, v in self.encoder.tokenizer.vocab.items() if "[unused" in k
+        )
+    
+    def get_token_id(self, token: str) -> int:
+        return self.encoder.tokenizer.vocab[token]
+
+    def get_characteristic_tensor_for_token_id(self, token_id: int):
+        return torch.tensor(
+            [i for i in range(len(self.encoder.tokenizer.vocab)) if i != token_id]
+        )
+
+    def freeze_weights(self):
+        for param in self.encoder.parameters():
+            param.requires_grad = False
+
+    def unfreeze_embedding_weigths(self):
+        for idx, p in enumerate(
+            self.encoder[0].auto_model.embeddings.word_embeddings.parameters()
+        ):
+            p.requires_grad = True
+
+    def get_eot_token_id(self) -> int:
+        return 102
+    
+    def get_parameters(self):
+        return self.encoder.parameters()
+    
+    def raw_encode(self, input_ids: torch.Tensor, attention_mask: torch.Tensor) -> torch.Tensor:
+        return self.encoder({
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+        })["sentence_embedding"]
+    
+    def get_embedding_for_token_id(self, token_id: int):
+        return self.encoder[0].auto_model.embeddings.word_embeddings.weight.data[token_id]
+    
+    def zero_grad_for_embeddings(self, mask: torch.Tensor):
+         self.encoder[0].auto_model.embeddings.word_embeddings.weight.grad[mask] = 0
+    
+    def get_tokenizer_vocab(self):
+        return self.encoder.tokenizer.vocab
+
     @property
     def name(self):
         return "Mini LM"
