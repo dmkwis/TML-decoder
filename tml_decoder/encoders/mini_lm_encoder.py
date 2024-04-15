760ebec4189ed76927bc57876e6cf17de02255a6
Jan Piotrowski <jan.piotrowski@mim-solutions.ai>
2024-04-15 23:16:47 +0200

    Add new methods
    In addition refactor old one due to pylint suggestions

--- tml_decoder/encoders/mini_lm_encoder.py
+++ tml_decoder/encoders/mini_lm_encoder.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 
 import torch
 from tml_decoder.encoders.abstract_encoder import AbstractEncoder
@@ -8,29 +8,33 @@ from numpy import ndarray
 
 from tml_decoder.utils.helper_functions import default_device
 
+
 class MiniLMEncoder(AbstractEncoder):
     def __init__(self, device=default_device()):
         super().__init__()
         self.device = device
-        self.encoder = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2").to(self.device)
-    
+        self.encoder = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2").to(
+            self.device
+        )
+
     def similarity(self, veca: ndarray, vecb: ndarray) -> float:
         return util.dot_score(veca, vecb).item()
-    
+
     def average_embedding(self, embeddings: ndarray) -> ndarray:
         summed = np.sum(embeddings, axis=0)
         return summed / np.linalg.norm(summed)
-    
+
     def encode(self, text: str) -> ndarray:
         result = self.encoder.encode(text, show_progress_bar=False)
         if isinstance(result, ndarray):
             return result
-        
-        raise TypeError("Expected ndarray from self.encoder.encode, got {}".format(type(result)))
-    
+
+        raise TypeError(
+            "Expected ndarray from self.encoder.encode, got {}".format(type(result))
+        )
+
     def encode_batch(self, texts: List[str]) -> List[ndarray]:
         return self.encoder.encode(texts, show_progress_bar=False)
-    
 
     def tokenize_text(self, text: str) -> List[int]:
         return self.encoder.tokenizer.encode(text)
@@ -39,7 +43,7 @@ class MiniLMEncoder(AbstractEncoder):
         return next(
             k for k, v in self.encoder.tokenizer.vocab.items() if "[unused" in k
         )
-    
+
     def get_token_id(self, token: str) -> int:
         return self.encoder.tokenizer.vocab[token]
 
@@ -52,7 +56,7 @@ class MiniLMEncoder(AbstractEncoder):
         for param in self.encoder.parameters():
             param.requires_grad = False
 
-    def unfreeze_embedding_weigths(self):
+    def unfreeze_embedding_weights(self):
         for idx, p in enumerate(
             self.encoder[0].auto_model.embeddings.word_embeddings.parameters()
         ):
@@ -60,24 +64,41 @@ class MiniLMEncoder(AbstractEncoder):
 
     def get_eot_token_id(self) -> int:
         return 102
-    
+
     def get_parameters(self):
         return self.encoder.parameters()
-    
-    def raw_encode(self, input_ids: torch.Tensor, attention_mask: torch.Tensor) -> torch.Tensor:
-        return self.encoder({
-            "input_ids": input_ids,
-            "attention_mask": attention_mask,
-        })["sentence_embedding"]
-    
+
+    def raw_encode(
+        self, input_ids: torch.Tensor, attention_mask: torch.Tensor
+    ) -> torch.Tensor:
+        return self.encoder(
+            {
+                "input_ids": input_ids,
+                "attention_mask": attention_mask,
+            }
+        )["sentence_embedding"]
+
     def get_embedding_for_token_id(self, token_id: int):
-        return self.encoder[0].auto_model.embeddings.word_embeddings.weight.data[token_id]
-    
+        return self.encoder[0].auto_model.embeddings.word_embeddings.weight.data[
+            token_id
+        ]
+
     def zero_grad_for_embeddings(self, mask: torch.Tensor):
-         self.encoder[0].auto_model.embeddings.word_embeddings.weight.grad[mask] = 0
-    
+        self.encoder[0].auto_model.embeddings.word_embeddings.weight.grad[mask] = 0
+
     def get_tokenizer_vocab(self):
         return self.encoder.tokenizer.vocab
+
+    def decode(self, token_ids: Union[int, List[int]]) -> str:
+        if isinstance(token_ids, int):
+            token_ids = [token_ids]
+        return self.encoder.tokenizer.decode(token_ids)
+
+    def train(self):
+        self.encoder.train()
+
+    def eval(self):
+        self.encoder.eval()
 
     @property
     def name(self):
